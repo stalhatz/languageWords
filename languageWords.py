d3a53ded2a822e6e0e684681567d7e7842b75d87
stalhatz <stalhatz@gmail.com>
2019-01-06 13:04:14 +0100

    Category / Word selection. Larousse/Wiktionary selection.

--- languageWords.py
+++ languageWords.py
@@ -4,6 +4,14 @@ import subprocess
 import datetime as dt
 import pandas as pd
 import pickle
+import argparse
+import sys
+
+from PyQt5.QtWidgets import (QApplication,QWidget,QMainWindow)
+from PyQt5.QtCore import (QUrl,QVariant,Qt,pyqtSignal)
+from PyQt5.QtCore import (QAbstractListModel,QModelIndex,QStringListModel)
+
+from ui_mainwindow import Ui_MainWindow
 
 class TextElement:
   def __init__(self, text , hyperlink = None , tag = None):
@@ -26,7 +34,7 @@ class TextElement:
 #     b = a.stdout.decode('utf-8')
 #     return b
   
-from PyQt5.QtWidgets import (QApplication,QInputDialog, QLineEdit,QCompleter)
+
 # def getQtTextInput(taskList, labelText):
 #   app = QApplication([])
 #   a = QInputDialog()
@@ -52,7 +60,7 @@ from PyQt5.QtWidgets import (QApplication,QInputDialog, QLineEdit,QCompleter)
 #     args =  title + ["--passivepopup"] + text
 #     runCommand("kdialog", args)
 
-import pandas as pd
+
 # def saveToPandas(tasks, taskName):
 #   global pandasTasks
 #   global pandasWindows
@@ -87,7 +95,9 @@ import pandas as pd
 #   taskTable = pd.DataFrame(tasksDict).T
 #   windowTable = pd.DataFrame(windowsDict).T
 #   return taskTable,windowTable
-      
+
+# self.webView = QtWebEngineWidgets.QWebEngineView(self.centralwidget)
+# from PyQt5 import QtWebEngineWidgets   
 def saveToPickle(a , filename):
   with open(filename, 'wb') as output:
     pickle.dump(a, output, pickle.HIGHEST_PROTOCOL)
@@ -107,8 +117,49 @@ def createDFFromDict(dictWords):
     words["tag"].append(dictWords[key][1])
   return pd.DataFrame.from_dict(words)
 
-import argparse
+class PandasTagList(QAbstractListModel):
+  tagChanged = pyqtSignal(str, name='tagChanged')
+  def __init__(self, df):
+    super(PandasTagList,self).__init__()
+    self.df = pd.pivot_table(df,values='text',index='tag',aggfunc=pd.Series.nunique).reset_index()
+    self.selectedIndex = 0
+  def rowCount(self, modelIndex):
+    #print("Rowcount() :: " + str(len(self.df)))
+    return len(self.df)
+  def data(self, index, role):
+    if not index.isValid() or not (0<=index.row()<len(self.df)):  return QVariant()
+    if role==Qt.DisplayRole:      return str(self.df.iloc[index.row(),0])
+  def selected(self, index , prevIndex):
+    self.selectedIndex = index.row()
+    self.tagChanged.emit( str(self.df.iloc[index.row(),0]) )
 
+
+class PandasWordList(QAbstractListModel):
+  dataChanged = pyqtSignal()
+  def __init__(self, df ,webview):
+    super(PandasWordList,self).__init__()
+    self.df = df
+    self.df_image = df
+    self.webView = webview
+    self.dict = "wiktionary"
+  def rowCount(self, modelIndex):
+    return len(self.df_image)
+  def data(self, index, role):
+    if not index.isValid() or not (0<=index.row()<len(self.df_image)):
+      return QVariant()
+    if role==Qt.DisplayRole:
+      return str(self.df_image.iloc[index.row(),0])
+  def selected(self, index , prevIndex):
+    if self.dict == "wiktionary":
+      url = QUrl("https://fr.wiktionary.org/wiki/" + str(self.df_image.iloc[index.row(),0]) )
+    elif self.dict == "larousse":
+      url = QUrl("https://www.larousse.fr/dictionnaires/francais/" + str(self.df_image.iloc[index.row(),0]) )
+    self.webView.load(url) 
+  def updateWords(self,tag):
+    self.df_image = self.df[self.df['tag'] == tag]
+    self.dataChanged.emit()
+  def updateDict(self,dictName):
+    self.dict = dictName
 words = {}
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Process some integers.')
@@ -126,4 +177,27 @@ if __name__ == "__main__":
       dictWords = loadFromPickle(args.l)
     except:
       print("Could not load from file")
-  print(dictWords)  
+  app = QApplication([])
+  
+  window = QMainWindow()
+  ui = Ui_MainWindow()
+  ui.setupUi(window)
+  ui.webView.load(QUrl("http://google.com"))
+  ui.dictSelect.insertItems(0,["wiktionary", "larousse"])
+  pwl = PandasWordList(dictWords,ui.webView)
+  ptl = PandasTagList(dictWords)
+  
+  #Set signals/slots
+  ui.tagview.setModel(ptl)
+  ui.tagview.selectionModel().currentChanged.connect(ptl.selected)
+  ui.wordview.setModel(pwl)
+  ui.wordview.selectionModel().currentChanged.connect(pwl.selected)
+  ui.dictSelect.currentTextChanged.connect(pwl.updateDict)
+
+  ptl.tagChanged.connect(pwl.updateWords)
+  pwl.dataChanged.connect(ui.wordview.reset)
+
+  
+
+  window.show()
+  app.exec_()
