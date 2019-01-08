ae687ea95a05d79ffe0710a99043b322ac18f12c
stalhatz <stalhatz@gmail.com>
2019-01-08 13:50:17 +0100

    Widget for showing scraped information from dictionaries.

--- languageWords.py
+++ languageWords.py
@@ -13,18 +13,18 @@ from PyQt5.QtCore import (QAbstractListModel,QModelIndex,QStringListModel)
 
 from ui_mainwindow import Ui_MainWindow
 
-class TextElement:
-  def __init__(self, text , hyperlink = None , tag = None):
-    self.text = text
-    self.link = "" 
-    self.date = dt.datetime.today()
-    self.tags = []
-    self.hyperlink = hyperlink
-    self.spellChecked = False
-  def __repr__(self):
-    return self.text
-  def __str__(self):
-    return self.text
+# class TextElement:
+#   def __init__(self, text , hyperlink = None , tag = None):
+#     self.text = text
+#     self.link = "" 
+#     self.date = dt.datetime.today()
+#     self.tags = []
+#     self.hyperlink = hyperlink
+#     self.spellChecked = False
+#   def __repr__(self):
+#     return self.text
+#   def __str__(self):
+#     return self.text
     
 
 # def runCommand(command, args):
@@ -61,43 +61,14 @@ class TextElement:
 #     runCommand("kdialog", args)
 
 
-# def saveToPandas(tasks, taskName):
-#   global pandasTasks
-#   global pandasWindows
-#   tasksDict = {}
-#   windowsDict = {}
-#   j = 0
-#   k=0
-#   for task in tasks.values():
-#     if len(task.subTasks) > 0:
-#       pandasTasks[taskName],pandasWindows[taskName] = saveToPandas(task.subTasks,str(task))
-    
-    
-# #    print(str(task) + " : Startdates :: " + str(len(task.startDates)) )
-# #    print(str(task) + " : Durations :: " + str(len(task.durations)) )
-#     for i,date in enumerate(task.startDates):
-#       taskDict = {}
-#       taskDict["taskName"] = str(task)
-#       taskDict["date"] = date
-#       taskDict["duration"] = task.durations[i]
-#       tasksDict[j] = taskDict
-#       j+=1
-      
-#     for i,processName in enumerate(task.processNames):
-#       windowDict= {}
-#       windowDict["taskName"] = str(task)
-#       windowDict["processName"] = processName
-#       windowDict["windowName"] = task.windowNames[i]
-#       windowDict["windowDate"] = task.windowDates[i]
-#       windowDict["windowDuration"] = task.windowDurations[i]
-#       windowsDict[k] = windowDict
-#       k+=1
-#   taskTable = pd.DataFrame(tasksDict).T
-#   windowTable = pd.DataFrame(windowsDict).T
-#   return taskTable,windowTable
-
 # self.webView = QtWebEngineWidgets.QWebEngineView(self.centralwidget)
 # from PyQt5 import QtWebEngineWidgets   
+
+import requests
+from bs4 import BeautifulSoup
+
+
+
 def saveToPickle(a , filename):
   with open(filename, 'wb') as output:
     pickle.dump(a, output, pickle.HIGHEST_PROTOCOL)
@@ -116,6 +87,26 @@ def createDFFromDict(dictWords):
     words["hyperlink"].append(dictWords[key][0])
     words["tag"].append(dictWords[key][1])
   return pd.DataFrame.from_dict(words)
+
+class DictWebList(QAbstractListModel):
+  dataChanged = pyqtSignal()
+  def __init__(self):
+    super(DictWebList,self).__init__()
+    self.definitionsList = []
+  def load(self,url):
+    self.definitionsList = []
+    html =  requests.get(url.toString()).text
+    s = BeautifulSoup(html,"html.parser")
+    for element in s.select("ol > li"):
+      self.definitionsList.append(element.text.split("\n")[0])
+    print(self.definitionsList)
+    self.dataChanged.emit()
+  def rowCount(self, modelIndex):
+    return len(self.definitionsList)
+  def data(self, index, role):
+    if not index.isValid() or not (0<=index.row()<len(self.definitionsList)):  return QVariant()
+    if role==Qt.DisplayRole:      return self.definitionsList[index.row()]
+
 
 class PandasTagList(QAbstractListModel):
   tagChanged = pyqtSignal(str, name='tagChanged')
@@ -136,6 +127,7 @@ class PandasTagList(QAbstractListModel):
 
 class PandasWordList(QAbstractListModel):
   dataChanged = pyqtSignal()
+  pageLoad    = pyqtSignal(QUrl)
   def __init__(self, df ,webview):
     super(PandasWordList,self).__init__()
     self.df = df
@@ -154,13 +146,18 @@ class PandasWordList(QAbstractListModel):
       url = QUrl("https://fr.wiktionary.org/wiki/" + str(self.df_image.iloc[index.row(),0]) )
     elif self.dict == "larousse":
       url = QUrl("https://www.larousse.fr/dictionnaires/francais/" + str(self.df_image.iloc[index.row(),0]) )
-    self.webView.load(url) 
+    #self.webView.load(url) 
+    self.pageLoad.emit(url)
   def updateWords(self,tag):
     self.df_image = self.df[self.df['tag'] == tag]
     self.dataChanged.emit()
   def updateDict(self,dictName):
     self.dict = dictName
 words = {}
+
+#def handleTabWidgetSignals():
+
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Process some integers.')
   parser.add_argument('-l', nargs='?', const='dictWords.pd.pkl', default=None)
@@ -182,12 +179,12 @@ if __name__ == "__main__":
   window = QMainWindow()
   ui = Ui_MainWindow()
   ui.setupUi(window)
-  ui.webView.load(QUrl("http://google.com"))
   ui.dictSelect.insertItems(0,["wiktionary", "larousse"])
   pwl = PandasWordList(dictWords,ui.webView)
   ptl = PandasTagList(dictWords)
-  
+  dwl = DictWebList()
   #Set signals/slots
+  ui.dictListView.setModel(dwl)
   ui.tagview.setModel(ptl)
   ui.tagview.selectionModel().currentChanged.connect(ptl.selected)
   ui.wordview.setModel(pwl)
@@ -196,8 +193,11 @@ if __name__ == "__main__":
 
   ptl.tagChanged.connect(pwl.updateWords)
   pwl.dataChanged.connect(ui.wordview.reset)
-
-  
+  dwl.dataChanged.connect(ui.dictListView.reset)
+  #Connect signals to web view
+  pwl.pageLoad.connect(ui.webView.load)
+  pwl.pageLoad.connect(dwl.load)
+  #ui.tabwidget.currentChanged.connect()
 
   window.show()
   app.exec_()
