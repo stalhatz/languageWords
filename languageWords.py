b1fc180ac28eb201e89e4e6c96040d752232519b
stalhatz <stalhatz@gmail.com>
2019-03-06 07:13:46 +0100

    TODOs

--- languageWords.py
+++ languageWords.py
@@ -6,6 +6,7 @@ from PyQt5.QtCore import QTimer
 #TODO: [FEATURE_0] Write README.md
 #TODO: [FEATURE_3] Exit program when closing WelcomeDialog
 #TODO: [FEATURE_3] Ensure project file has a .pkl extension
+#TODO: [FEATURE_3] Have a default metatag "All" for all tags. Should not be renamable or deletable.
 #TODO: [FEARURE_2] Request asynchronously more than one web page
 #TODO: [FEATURE_1] Implement a caching scheme to limit reloading.
 #TODO: [FEATURE_1] Sort ListViews by interacting with their corresponding Label
