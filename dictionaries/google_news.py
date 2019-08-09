36e5844942f41dd3af16ff4e740bc909086692c7
stalhatz <stalhatz@gmail.com>
2019-08-09 10:08:37 +0200

    [FEATURE] Added language and country result arguments in google news requests

--- dictionaries/google_news.py
+++ dictionaries/google_news.py
@@ -1,6 +1,8 @@
 
 name = "googleNews"
 languages = ["English","French"] 
+langCodes = ["EN","FR"]
+countryCodes = ["US","FR"]
 import dict_types as dt
 from bs4 import BeautifulSoup
 from collections import namedtuple
@@ -8,7 +10,7 @@ from collections import namedtuple
 def createUrl(word,requestLang):
   for i,lang in enumerate(languages):
     if requestLang == lang:
-      return "https://www.google.com/search?q=\""+ word.lower() +"\"&tbm=nws" 
+      return "https://www.google.com/search?q=\""+ word.lower() +"\"&tbm=nws" + "&cr=country"+countryCodes[i] + "&lr=lang"+langCodes[i]
   raise ValueError("Could not find " + str(requestLang) + " in " + str(name) + "'s language list. Available langs : " + str(languages) )
 
 def getUrlFromHrefArg(hrefArg):
