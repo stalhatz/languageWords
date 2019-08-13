b387998dd923d768c2915ef33234594df0740924
stalhatz <stalhatz@gmail.com>
2019-08-13 14:09:12 +0200

    [FIXED] Localization in google news

--- dictionaries/google_news.py
+++ dictionaries/google_news.py
@@ -10,7 +10,7 @@ from collections import namedtuple
 def createUrl(word,requestLang):
   for i,lang in enumerate(languages):
     if requestLang == lang:
-      return "https://www.google.com/search?q=\""+ word.lower() +"\"&tbm=nws" + "&cr=country"+countryCodes[i] + "&lr=lang"+langCodes[i]
+      return "https://www.google.com/search?q=\""+ word.lower() +"\"&tbm=nws" + "&cr=country"+countryCodes[i] + "&lr=lang_"+langCodes[i]+ "&hl="+langCodes[i]
   raise ValueError("Could not find " + str(requestLang) + " in " + str(name) + "'s language list. Available langs : " + str(languages) )
 
 def getUrlFromHrefArg(hrefArg):
